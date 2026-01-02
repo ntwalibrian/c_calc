f60a6cff165fca4715761857f625de5def8e8526
ntwalibrian <ntwalibriand@gmail.com>
2026-01-02 23:40:21 +0200

    variables

--- main.c
+++ main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    printf("hello, world");
+
+    // this is my first program :)
+
+    /*
+    this is also  a 
+    comment
+    */
+
+    int age = 25;
+    int year = 2025;
+    int quantity = 1;
+
+    float gpa = 2.5;
+    float price = 19.99;
+    float temperature = -10.1;
+
+    printf("price is %f \n", price);
+    printf("the temperature is %f celcius\n", temperature);
+    printf("your gpa %f \n", gpa);
+
+    printf("you have ordered %d x items", quantity);
+    printf("year is %d  \n", year);
+    printf("you are %d years old\n", age);
+    printf("hello, world\n");
+    printf("diouf\n");
     return 0;
 }
