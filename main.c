487df928e9da1f500e6c3497b2f81132dc209466
ntwalibrian <ntwalibriand@gmail.com>
2026-01-09 19:32:14 +0200

    arrays 2

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    
-    int numbers[] = {10, 20, 30, 40, 50, 60};
-    char grades[]= {'A','B','C','D'};
-    char name[] = "Bro code";
 
-
-    int numbers[] = {10, 20, 30, 40, 50, 60};
+    int numbers[5] = {0};
     int size = sizeof(numbers) / sizeof(numbers[0]);
 
-    for (int i = 0; i < size; i++) {
-        printf("%d\n",numbers[i]);
+    for (int i = 0; i < size; i++) 
+    {
+        printf("Enter a value: ");
+        scanf("%d",&numbers[i]);
     }
+
+    for (int i = 0; i < size; i++) 
+    {
+        printf("%d\n");
+        
+    }
+
 
     return 0;
 
