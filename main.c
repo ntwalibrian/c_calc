7058a985cf08bbb22b10d3298bd04b8d29d5e1b3
ntwalibrian <ntwalibriand@gmail.com>
2026-01-11 17:12:19 +0200

    ternary operators

--- main.c
+++ main.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <stdbool.h>
+
 
 int main() {
 
-    int x = 5;
-    int y = 6;
-    int max = (x >= y) ? x : y;
-    printf("%d", max);
+    // int x = 15;
+    // int y = 6;
+    // int max = (x >= y) ? x : y;
 
+    // bool isOnline = true;
+    // printf("%s", (isOnline) ? "he is online" : "he is offline");
+
+    
     return 0;
 
 }
